f74e2eb3e4495a8a9ba2c7af5292825bcffb4234
SuhHyeonjun <dmsgktn0208@gmail.com>
2023-12-12 17:45:12 +0900

    refactor : 숫자 범위를 반복문을 탐색하지 않고 정규식으로 범위를 벗어나는 숫자가 하나 이상 있는지 확인하도록 수정

--- src/main/java/baseball/view/validator/PlayerNumberValidator.java
+++ src/main/java/baseball/view/validator/PlayerNumberValidator.java
@@ -10,7 +10,7 @@ public class PlayerNumberValidator {
 
     public static void validate(String input) {
         validateFormat(input);
-        validateSize(input);
+        validateSize(input)가
         validateNumberRange(input);
     }
 
@@ -22,17 +22,14 @@ public class PlayerNumberValidator {
 
     private static void validateSize(String input) {
         String[] numbers = StringConvertor.splitByBlank(input);
-        if (numbers.length > MAX_SIZE) {
+        if (numbers.length != MAX_SIZE) {
             throw new IllegalArgumentException("3자리의 숫자를 입력해야합니다.");
         }
     }
 
     private static void validateNumberRange(String input) {
-        String[] numbers = StringConvertor.splitByBlank(input);
-        for (String number : numbers) {
-            if (!RegexPattern.ONE_TO_NINE.matches(number)) {
-                throw new IllegalArgumentException("1 ~ 9 범위의 숫자만 입력할 수 있습니다.");
-            }
+        if (!RegexPattern.ONE_TO_NINE.matches(input)) {
+            throw new IllegalArgumentException("1 ~ 9 범위의 숫자만 입력할 수 있습니다.");
         }
     }
 }
