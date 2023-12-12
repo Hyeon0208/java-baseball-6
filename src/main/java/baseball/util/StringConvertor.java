1ba243b19a8e225a0202e55dc018afe06cc9274b
SuhHyeonjun <dmsgktn0208@gmail.com>
2023-12-12 12:41:22 +0900

    feat : 문자열 변환기 클래스 추가

---
+++ src/main/java/baseball/util/StringConvertor.java
@@ -0,0 +1,11 @@
+package baseball.util;
+
+public class StringConvertor {
+
+    private StringConvertor() {
+    }
+
+    public static String[] splitByBlank(String value) {
+        return value.split("");
+    }
+}
