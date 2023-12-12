ab660c261d442a34bbdbe0828f1311a6bff1333e
SuhHyeonjun <dmsgktn0208@gmail.com>
2023-12-12 17:09:39 +0900

    feat : 게임 값들을 상수로 갖는 클래스 추가

--- src/main/java/baseball/constant/BaseBallValue.java
+++ src/main/java/baseball/constant/BaseBallValue.java
@@ -1,6 +1,6 @@
 package baseball.constant;
 
-public class BseBallValue {
+public class BaseBallValue {
     public static final String RETRY_COMMAND = "1";
     public static final String QUIT_COMMAND = "2";
 }
