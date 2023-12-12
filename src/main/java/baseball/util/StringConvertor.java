6a7de8bb27e4c1c8ef011454c24d4d77c2876d28
SuhHyeonjun <dmsgktn0208@gmail.com>
2023-12-12 17:52:18 +0900

    refactor : 구분자 상수화

--- src/main/java/baseball/util/StringConvertor.java
+++ src/main/java/baseball/util/StringConvertor.java
@@ -4,12 +4,13 @@ import java.util.Arrays;
 import java.util.List;
 
 public class StringConvertor {
+    private static final String SEPARATOR = "";
 
     private StringConvertor() {
     }
 
     public static String[] splitByBlank(String value) {
-        return value.split("");
+        return value.split(SEPARATOR);
     }
 
     public static List<Integer> StringNumberToIntegerList(String value) {
