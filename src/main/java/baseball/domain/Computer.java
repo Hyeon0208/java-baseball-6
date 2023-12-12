f9a9fd0cd37e754ff6c05f624a345b6a7e6e78ad
SuhHyeonjun <dmsgktn0208@gmail.com>
2023-12-12 13:39:23 +0900

    feat : 컴퓨터 객체 추가

---
+++ src/main/java/baseball/domain/Computer.java
@@ -0,0 +1,23 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class Computer {
+    private List<Integer> numbers;
+
+    public Computer(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+
+    public int getNumberBy(int index) {
+        return numbers.get(index);
+    }
+
+    public boolean contains(int number) {
+        return numbers.contains(number);
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+}
